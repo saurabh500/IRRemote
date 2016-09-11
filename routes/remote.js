6d00287395659773f6af5301f75c0695b22c8927
Saurabh Singh <sausing@microsoft.com>
2016-09-11 21:15:23 +0000

    Cleaning up the remote rout

--- routes/remote.js
+++ routes/remote.js
@@ -2,22 +2,11 @@ var express = require('express');
 var router = express.Router();
 var exec = require('child_process').exec;
 
-/*
-router.param('command', function (req, res, next, command) {
-  console.log('Command ' +  command);
-  next();
-});
-*/
-
-router.get('/MyCommand', function(req, res, next) {
-  res.send(" MyCommand Called ");
-});
-
 router.get('/:command', function(req, res) {
     
     var command = req.params.command;
     
-    console.log(command);
+    console.log("Command Called " + command);
     exec('irsend SEND_ONCE SAMSUNG55 ' + command, function callback(error, stdout, stderr){
         if(error){
             console.log(error);
@@ -29,9 +18,5 @@ router.get('/:command', function(req, res) {
     });
 });
 
-router.get('/', function(req, res, next) {
-    console.log("Root Called for Remote ");
-    
-});
 
 module.exports = router;
