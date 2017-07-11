cd152eb3efc6ba6d58a4bad7b058e5dba8eebf07
Saurabh Singh <sausing@microsoft.com>
2017-07-11 23:05:28 +0000

    add multi step volume increase

--- routes/remote.js
+++ routes/remote.js
@@ -7,6 +7,24 @@ router.get('/:command', function(req, res) {
     var command = req.params.command;
     
     console.log("Command Called " + command);
+    if(command === "KEY_VOLUMEUP_MULTI")
+    {
+	for(i=0;i < 5 ; i++ )
+	{
+	    exec('irsend SEND_ONCE SAMSUNG55 KEY_VOLUMEUP'); 
+	}
+	res.send(req.params);
+	return;
+    }
+
+    if(command === "KEY_VOLUMEDOWN_MULTI")
+    {
+        for( i = 0 ; i < 5 ; i++ ){
+		exec('irsend SEND_ONCE SAMSUNG55 KEY_VOLUMEDOWN');
+	}
+	res.send(req.params);
+	return;
+    }
     exec('irsend SEND_ONCE SAMSUNG55 ' + command, function callback(error, stdout, stderr){
         if(error){
             console.log(error);
